20a2df464f6f88974ad9a9dabb548165cc7b491a
Admin <reteamboss@gmail.com>
2024-08-26 21:55:21 +0300

    Задание 4

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,18 @@
 fun main() {
-    println("Hello World!")
+    var num = 13
+
+
+    if (num > 0 && num % 2 == 0) {
+        println("Положительное четное число")
+    } else if (num > 0 && num % 2 != 0){
+        println("Положительное нечетное число")
+    } else if (num < 0 && num % 2 == 0){
+        println("Отрицательное четное число")
+    } else if (num < 0 && num % 2 != 0){
+        println("Отрицательное нечетное число")
+    } else if (num == 0){
+        println("Нулевое число")
+    } else {
+        println("Такого не знаю")
+    }
 }
